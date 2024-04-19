de05a8e83319ec3430c54f2ef38455376df529cb
Kluvens <z5339252@ad.unsw.edu.au>
2024-04-19 14:48:40 +1000

    updated multithreading

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use clap::value_parser;
 use rsheet_lib::cell_value::{self, CellValue};
 use rsheet_lib::command_runner::{ CellArgument, CommandRunner };
 use rsheet_lib::connect::{Manager, Reader, Writer};
@@ -5,76 +6,96 @@ use rsheet_lib::replies::Reply;
 
 use std::collections::HashMap;
 use std::error::Error;
-use std::ops::Rem;
-use std::sync::{Arc, Mutex};
+use std::sync::mpsc::RecvError;
+use std::sync::{mpsc, Arc, Mutex};
+use std::{cell, result, thread, vec};
+
+extern crate regex;
+use regex::Regex;
 
 use log::info;
 
-pub struct SpreadsheetManager {
-    cells: Arc<Mutex<HashMap<String, CellValue>>>,
+pub fn handle_message(message: &str, cells: &Arc<Mutex<HashMap<String, CellValue>>>) -> Option<Reply> {
+    let parts: Vec<&str> = message.trim().split_whitespace().collect();
+    match parts[0] {
+        "get" if parts.len() == 2 => {
+            let cell_name = parts[1];
+            let cells = cells.lock().unwrap();
+            match cells.get(cell_name) {
+                Some(value) => Some(Reply::Value(cell_name.to_string(), value.clone())),
+                None => Some(Reply::Value(cell_name.to_string(), CellValue::None)),
+            }
+        },
+        "set" if parts.len() >= 3 => {
+            let cell_name = parts[1].to_string();
+            let parts_var = parts[2..].to_vec();
+            let parts_var_str = parts_var.join(" ");
+            let mut variables = HashMap::new();
+
+            for part in parts {
+                if part.contains("_") {
+                    if let Some(matrix_arg) = parse_matrix_variable(part, cells) {
+                        variables.insert(remove_sum_expression(part), matrix_arg);
+                    } else if let Some(vector_arg) = parse_matrix_variable(part, cells) {
+                        variables.insert(remove_sum_expression(part), vector_arg);
+                    }
+                } else {
+                    if let Ok(value) = part.parse::<i64>() {
+                        variables.insert(remove_sum_expression(part), CellArgument::Value(CellValue::Int(value)));
+                    } else if let Some(value) = cells.lock().unwrap().get(part) {
+                        variables.insert(remove_sum_expression(part), CellArgument::Value(value.clone()));
+                    }
+                }
+            }
+            
+            let value = CommandRunner::new(&remove_sum_expression(&parts_var_str)).run(&variables);
+
+            {
+                let mut cells = cells.lock().unwrap();
+                cells.insert(cell_name, value);
+            }
+            None
+        },
+        _ => Some(Reply::Error("Invalid command".to_string())),
+    }
 }
 
-impl SpreadsheetManager {
-    pub fn new() -> Self {
-        Self { cells: Arc::new(Mutex::new(HashMap::new())) }
-    }
+fn parse_matrix_variable(variable: &str, cells: &Arc<Mutex<HashMap<String, CellValue>>>) -> Option<CellArgument> {
+    todo!()
+}
 
-    pub fn handle_message(&self, message: &str) -> Option<Reply> {
-        let parts: Vec<&str> = message.trim().split_whitespace().collect();
-        match parts[0] {
-            "get" if parts.len() == 2 => {
-                let cell_name = parts[1];
-                let cells = self.cells.lock().unwrap();
-                let cell_value = cells.get(cell_name);
-                match cell_value {
-                    Some(value) => Some(Reply::Value(cell_name.to_string(), value.clone())),
-                    None => Some(Reply::Value(cell_name.to_string(), CellValue::None)),
-                }
-            },
-            "set" if parts.len() >= 3 => {
-                let cell_name = parts[1].to_string();
-                let value_str = parts[2..].join(" ");
-                let mut cells = self.cells.lock().unwrap();
-                let variables = cells.iter()
-                    .map(|(key, value)| (key.clone(), CellArgument::Value(value.clone())))
-                    .collect::<HashMap<String, CellArgument>>();
-                let value = if let Ok(num) = value_str.parse::<i64>() {
-                    CellValue::Int(num)
-                } else {
-                    CommandRunner::new(&value_str).run(&variables)
-                };
-
-                cells.insert(cell_name, value);
-                None
-            },
-            _ => Some(Reply::Error("Invalid command".to_string())),
-        }
-    }
+fn remove_sum_expression(input: &str) -> String {
+    let re = Regex::new(r"sum\(([^)]+)\)").unwrap();
+    re.replace_all(input, "$1").to_string()
 }
 
 pub fn start_server<M>(mut manager: M) -> Result<(), Box<dyn Error>>
 where
     M: Manager,
 {
-    let spreadsheet_manager = SpreadsheetManager::new();
-    let (mut recv, mut send) = manager.accept_new_connection().unwrap();
+    let cells: Arc<Mutex<HashMap<String, CellValue>>> = Arc::new(Mutex::new(HashMap::new()));
     loop {
-        info!("Just got message");
-        let msg = match recv.read_message() {
-            Ok(msg) => {
-                msg
+        match manager.accept_new_connection() {
+            Ok(result) => {
+                let (mut recv, mut send) = result;
+                info!("Just got message");
+                let cloned_cells = Arc::clone(&cells);
+                thread::scope(|s| {
+                    s.spawn(move || {
+                        loop {
+                            let msg = match recv.read_message() {
+                                Ok(msg) => msg,
+                                Err(_) => return,
+                            };
+                            let reply = handle_message(&msg, &cloned_cells);
+                            if let Some(reply_msg) = reply {
+                                send.write_message(reply_msg);
+                            }
+                        }
+                    });
+                })
             },
-            Err(_) => {
-                return Ok(());
-            }
-        };
-        let reply = spreadsheet_manager.handle_message(&msg);
-        match reply {
-            Some(replay_msg) => {
-                send.write_message(replay_msg)?;
-            },
-            None => {
-            }
+            Err(_) => return Ok(()),
         }
     }
 }
